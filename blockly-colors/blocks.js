c98fe85157e6d4bc96dacc82e2fede1dbe5e2c95
Jensen Bernard <jensenbernard5@gmail.com>
2016-07-19 09:15:01 +0200

    moved blockly-colors

--- blockly-colors/blocks.js
+++ blockly-colors/blocks.js
@@ -3,7 +3,20 @@
  *  This code will link the BLOCKLY workspace to a CSS file for better
  *  customization. This file should be required AFTER the BLOCKLY libs and
  *  also requires JQUERY.
+ *
+ *  Blockly-colors makes it possible to link custom CSS files to the basic
+ *  BLOCKLY workspace. The original styles will be overwritten! You can better
+ *  start from the template in this folder and only customize in the
+ *  CUSTOMIZE section.
+ *
+ *  You can call this function like this:
+ *
+ *    customWorkspace('css/blocks.css', function() {
+ *      Blockly.inject ... (etc)
+ *    });
+ *
  */
+
 var customWorkspace = function(css, callback) {
   $.ajax({ url: css, method: 'GET', success: function(result) {
     Blockly.Css.CONTENT = result.replace(/(\r\n)|\r|\n/g, '\n').split(/\n+/g);
