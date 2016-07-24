a03c8e2af89946a8c477a3e4a366c76e1ff54c0f
Jensen Bernard <jensenbernard5@gmail.com>
2016-07-24 20:42:18 +0200

    Added ColorScheme

---
+++ scripts/Colors.js
@@ -0,0 +1,31 @@
+/**
+ *  Here are the main color schemes defined that can be reused over and over
+ *  again. For the main part these will be the colors for different graphs
+ *  with every color corresponding a player. This means the nth player will
+ *  have the nth color in this list. So if you want to change the order of the
+ *  colors in the graphs, just change them in this file. We used the main
+ *  color schemes of flatuicolors.com.
+ */
+
+const GRAPH_COLORS = [
+  'rgb(41, 187, 156)',    // TURQUOISE
+  'rgb(154, 92, 180)',    // AMETHYST
+  'rgb(53, 73, 93)',      // WET ASPHALT
+  'rgb(57, 202, 116)',    // EMERALD
+  'rgb(229, 77, 66)',     // ALIZARIN
+  'rgb(58, 153, 216)',    // PETER RIVER
+  'rgb(228, 126, 48)',    // CARROT
+  'rgb(127, 140, 141)',   // ASBESTOS
+  'rgb(240, 195, 48)',    // SUN FLOWER
+  'rgb(149, 165, 166)',   // CONCRETE
+  'rgb(35, 159, 133)',    // GREEN SEA
+  'rgb(141, 72, 171)',    // WISTERIA
+  'rgb(48, 173, 99)',     // NEPHRITIS
+  'rgb(189, 195, 199)',   // SILVER
+  'rgb(241, 155, 44)',    // ORANGE
+  'rgb(45, 62, 79)',      // MIDNIGHT BLUE
+  'rgb(47, 129, 183)',    // BELIZE HOLE
+  'rgb(251, 252, 252)',   // CLOUDS
+  'rgb(190, 58, 49)',     // POMEGRANATE
+  'rgb(209, 84, 25)'      // PUMPKIN
+];
